2154665862d5e93429708957018c99e1519e3b55
ZahraaDk <zahraadokmak@gmail.com>
2023-10-22 23:45:59 +0300

    added sentiment score column to fact table

--- Sql_Commands/V03_hook_create_fact_table.sql
+++ Sql_Commands/V03_hook_create_fact_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS dw_reporting.fct_booking 
+CREATE TABLE IF NOT EXISTS target_schema.fct_booking 
 (
     review_id BIGINT,
     listing_id TEXT,
@@ -8,12 +8,13 @@ CREATE TABLE IF NOT EXISTS dw_reporting.fct_booking
     price NUMERIC, 
     number_of_reviews TEXT, 
     review_scores_rating NUMERIC,
+    sentiment_score NUMERIC,
     PRIMARY KEY (listing_id, review_id)
 );
 
-CREATE INDEX IF NOT EXISTS "idx_fct_listing" ON dw_reporting.fct_booking(listing_id);
+CREATE INDEX IF NOT EXISTS "idx_fct_listing" ON target_schema.fct_booking(listing_id);
 
-INSERT INTO dw_reporting.fct_booking
+INSERT INTO target_schema.fct_booking
 SELECT DISTINCT
     src_reviews.review_id, 
     src_listing.listing_id, 
@@ -22,9 +23,10 @@ SELECT DISTINCT
     src_listing.neighbourhood_view, 
     src_listing.price, 
     src_listing.number_of_reviews, 
-    src_listing.review_scores_rating 
-FROM dw_reporting.stg_cleaned_df1 as src_listing
-INNER JOIN dw_reporting.stg_cleaned_df2 as src_reviews
+    src_listing.review_scores_rating,
+    src_reviews.sentiment_score
+FROM target_schema.stg_cleaned_df1 as src_listing
+INNER JOIN target_schema.stg_cleaned_df2 as src_reviews
     ON src_listing.listing_id = src_reviews.listing_id
 
 ON CONFLICT (listing_id, review_id) DO UPDATE
@@ -34,4 +36,5 @@ SET
     neighbourhood_view = EXCLUDED.neighbourhood_view, 
     price = EXCLUDED.price, 
     number_of_reviews = EXCLUDED.number_of_reviews, 
-    review_scores_rating = EXCLUDED.review_scores_rating;
+    review_scores_rating = EXCLUDED.review_scores_rating, 
+    sentiment_score = EXCLUDED.sentiment_score;
