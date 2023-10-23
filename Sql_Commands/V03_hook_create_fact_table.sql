13be19da74bca23ad2345d198e01846561de1c0e
ZahraaDk <zahraadokmak@gmail.com>
2023-10-23 12:55:01 +0300

    edited fact table query to include sentiment

--- Sql_Commands/V03_hook_create_fact_table.sql
+++ Sql_Commands/V03_hook_create_fact_table.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS target_schema.fct_booking 
 (
-    review_id BIGINT,
+    review_id BIGINT PRIMARY KEY,
     listing_id TEXT,
     last_updated TIMESTAMP, 
+    reviewer_id INTEGER,
     host_id BIGINT, 
     neighbourhood_view TEXT, 
     price NUMERIC, 
     number_of_reviews TEXT, 
     review_scores_rating NUMERIC,
-    sentiment_score NUMERIC,
-    PRIMARY KEY (listing_id, review_id)
+    sentiment_score NUMERIC
 );
 
 CREATE INDEX IF NOT EXISTS "idx_fct_listing" ON target_schema.fct_booking(listing_id);
@@ -18,7 +18,8 @@ INSERT INTO target_schema.fct_booking
 SELECT DISTINCT
     src_reviews.review_id, 
     src_listing.listing_id, 
-    src_listing.last_updated,
+    src_reviews.last_updated,
+    src_reviews.reviewer_id,
     src_listing.host_id, 
     src_listing.neighbourhood_view, 
     src_listing.price, 
@@ -29,9 +30,11 @@ FROM target_schema.stg_cleaned_df1 as src_listing
 INNER JOIN target_schema.stg_cleaned_df2 as src_reviews
     ON src_listing.listing_id = src_reviews.listing_id
 
-ON CONFLICT (listing_id, review_id) DO UPDATE
+ON CONFLICT (review_id) DO UPDATE
 SET 
+    listing_id = EXCLUDED.listing_id,
     last_updated = EXCLUDED.last_updated, 
+    reviewer_id = EXCLUDED.reviewer_id,
     host_id = EXCLUDED.host_id, 
     neighbourhood_view = EXCLUDED.neighbourhood_view, 
     price = EXCLUDED.price, 
