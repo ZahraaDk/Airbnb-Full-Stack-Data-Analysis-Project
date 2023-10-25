104959f9efdcaece5bbd45a03a5241e82aed44d1
ZahraaDk <zahraadokmak@gmail.com>
2023-10-25 20:10:01 +0300

    Edited the fact table

--- Sql_Commands/V03_hook_create_fact_table.sql
+++ Sql_Commands/V03_hook_create_fact_table.sql
@@ -2,15 +2,17 @@ CREATE TABLE IF NOT EXISTS target_schema.fct_booking
 (
     review_id BIGINT PRIMARY KEY,
     listing_id TEXT,
-    last_updated TIMESTAMP, 
+    booking_date TIMESTAMP, 
     reviewer_id INTEGER,
     host_id BIGINT, 
     neighbourhood_view TEXT, 
     price NUMERIC, 
     number_of_reviews TEXT, 
     review_scores_rating NUMERIC,
-    sentiment_score NUMERIC
-);
+    sentiment_score NUMERIC, 
+    longitude NUMERIC, 
+    latitude NUMERIC
+); 
 
 CREATE INDEX IF NOT EXISTS "idx_fct_listing" ON target_schema.fct_booking(listing_id);
 
@@ -18,14 +20,16 @@ INSERT INTO target_schema.fct_booking
 SELECT DISTINCT
     src_reviews.review_id, 
     src_listing.listing_id, 
-    src_reviews.last_updated,
+    src_reviews.booking_date,
     src_reviews.reviewer_id,
     src_listing.host_id, 
     src_listing.neighbourhood_view, 
     src_listing.price, 
     src_listing.number_of_reviews, 
     src_listing.review_scores_rating,
-    src_reviews.sentiment_score
+    src_reviews.sentiment_score, 
+    src_listing.longitude, 
+    src_listing.latitude
 FROM target_schema.stg_cleaned_df1 as src_listing
 INNER JOIN target_schema.stg_cleaned_df2 as src_reviews
     ON src_listing.listing_id = src_reviews.listing_id
@@ -33,11 +37,13 @@ INNER JOIN target_schema.stg_cleaned_df2 as src_reviews
 ON CONFLICT (review_id) DO UPDATE
 SET 
     listing_id = EXCLUDED.listing_id,
-    last_updated = EXCLUDED.last_updated, 
+    booking_date = EXCLUDED.booking_date, 
     reviewer_id = EXCLUDED.reviewer_id,
     host_id = EXCLUDED.host_id, 
     neighbourhood_view = EXCLUDED.neighbourhood_view, 
     price = EXCLUDED.price, 
     number_of_reviews = EXCLUDED.number_of_reviews, 
     review_scores_rating = EXCLUDED.review_scores_rating, 
-    sentiment_score = EXCLUDED.sentiment_score;
+    sentiment_score = EXCLUDED.sentiment_score, 
+    longitude = EXCLUDED.longitude, 
+    latitude = EXCLUDED.latitude;
