e0036eccd7494f8d7ca9a00cc8eaf7d6888b845c
ZahraaDk <zahraadokmak@gmail.com>
2023-10-29 16:23:32 +0200

    Added logging and monitoring to the prehook

--- prehook.py
+++ prehook.py
@@ -4,6 +4,7 @@ from pandas_handler import dataframes_cleansed
 from misc_handler import retrieve_sql_files
 from lookups import ErrorHandling, SQLCommands, DestSchema, PrehookSteps
 import os 
+import logging
 
 def execute_sql_folder_prehook(db_session, target_schema = DestSchema.DW_SCHEMA, sql_directory_path = SQLCommands.SQL_FOLDER):
     sql_files = None
@@ -15,35 +16,34 @@ def execute_sql_folder_prehook(db_session, target_schema = DestSchema.DW_SCHEMA,
                     sql_query = file.read() 
                     sql_query = sql_query.replace('target_schema', target_schema.value)
                     return_query = execute_query(db_session=db_session, query=sql_query)
-                    print("Prehook SQL Commands were successfully executed!")
                     if not return_query == ErrorHandling.NO_ERROR:
                         raise Exception( f" {PrehookSteps.EXECUTE_SQL_COMMANDS.value} = SQL File Error on SQL file = " + str(sql_file))
+        logging.info("Prehook SQL folder was successfully executed!")
+
     except Exception as e:
         show_error_message(ErrorHandling.PREHOOK_SQL_ERROR.value, str(e))
     finally:
         return sql_files 
+
     
 def create_sql_staging_tables(db_session, target_schema):
     dataframes_dict  = dataframes_cleansed()
-    print("Data cleaning and optimization is fully complete!")
+    logging.info("Data cleaning and optimization was fully completed.")
     create_statements = {}
-
     for table_name, source_df in dataframes_dict.items():
         if source_df.empty:
             raise Exception(f"No DataFrame returned for table '{table_name}'")
-
         try:
             stg_table_name = f"stg_{table_name}"
             staging_df = source_df.head(1)
             columns = list(staging_df.columns)
             create_stmt = create_statement_from_df(staging_df, f"{target_schema.value}", stg_table_name)
             execute_return_val = execute_query(db_session=db_session, query=create_stmt)
-            print("The create staging table statement was successfully executed!")
             if execute_return_val != ErrorHandling.NO_ERROR:
                 raise Exception(f"{ErrorHandling.EXECUTE_QUERY_ERROR}: this error occured while creating the table!")
             create_sql_stg_table_idx(db_session, f"{target_schema.value}", stg_table_name, columns[0])
-            print("Staging tables index was created!")
             create_statements[table_name] = create_stmt
+            logging.info("The create staging table statement and staging table index was successfully created!")
         except Exception as e:
             show_error_message(ErrorHandling.ERROR_CREATING_STAGING_TABLE.value, str(e))
         
@@ -57,7 +57,6 @@ def create_sql_stg_table_idx(db_session,source_name,table_name,index_val):
         show_error_message(PrehookSteps.CREATE_TABLE_IDX.value, str(e))
 
 def execute_prehook(sql_directory_path = SQLCommands.SQL_FOLDER):
-    step = None
     try:
         db_session = create_connection()
         execute_sql_folder_prehook(db_session,DestSchema.DW_SCHEMA,sql_directory_path)
