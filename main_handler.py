63d04911512aa0d283fc061214fa8238df6ff7ea
ZahraaDk <zahraadokmak@gmail.com>
2023-10-29 16:23:01 +0200

    added the logging config

--- main_handler.py
+++ main_handler.py
@@ -1,7 +1,10 @@
 from prehook import execute_prehook
 from hook import execute_hook
 from posthookk import execute_posthook
+import logging 
+
+logging.basicConfig(filename='execution.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 execute_prehook()
-execute_hook()
-execute_posthook()
+# execute_hook()
+# execute_posthook()
